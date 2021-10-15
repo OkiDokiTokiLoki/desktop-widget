6f3e9af991321c5aec4771f363b49a4002ba66a4
tokiloki <codebadger1@gmail.com>
2021-10-15 10:16:27 +0200

    play functionality added

--- javascript/relax.js
+++ javascript/relax.js
@@ -7,3 +7,32 @@ const timeDisplay = document.querySelector(".display");
 const outlineLength = outline.getTotalLength();  // Refering to the outline of the progress bar
 const timeSelect = document.querySelectorAll(".timer button");
 let someDuration = 600;  // The duration is not based on the duration of each song but rather one being set by us as a standard
+
+outline.style.strokeDashoffset = outlineLength;
+outline.style.strokeDasharray = outlineLength;
+timeDisplay.textContent = `${Math.floor(someDuration / 60)}:${Math.floor(someDuration % 60)}0`;
+
+sounds.forEach(sound => {
+    sound.addEventListener("click", function() {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+        checkPlaying(song);
+    });
+});
+
+play.addEventListener("click", function() {
+    checkPlaying(song);
+});
+
+// Play/Pause function
+const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      video.play();
+      play.src = "./img/pause.svg";
+    } else {
+      song.pause();
+      video.pause();
+      play.src = "./img/play.svg";
+    }
+};
